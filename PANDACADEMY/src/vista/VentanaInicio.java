f470e87cf889acbc03b064444ee0d77a4c775eda
Juan Pablo Gutierrez <juan.pis.01@hotmail.com>
2020-11-18 15:40:35 -0500

    Configurando fx

---
+++ PANDACADEMY/src/vista/VentanaInicio.java
@@ -0,0 +1,20 @@
+package vista;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VentanaInicio extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage window) throws Exception {
+		window.setTitle("Ventana de inicio");
+		window.show();
+		
+	}
+
+}
