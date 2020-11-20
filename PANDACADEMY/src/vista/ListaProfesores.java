437bd7a14516ed50e10ee7790bcbc50c8a85528c
Jurregoz <jonathanurrego@hotmail.com>
2020-11-20 11:20:31 -0500

    Update ListaProfesores.java

--- PANDACADEMY/src/vista/ListaProfesores.java
+++ PANDACADEMY/src/vista/ListaProfesores.java
@@ -1,6 +1,17 @@
 package vista;
+import gestorAplicacion.Persona.*;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class ListaProfesores {
+	
+	VBox lista;
+	
+	public ListaProfesores(Profesor profesor) {
+		lista = new VBox(new TextField());
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
