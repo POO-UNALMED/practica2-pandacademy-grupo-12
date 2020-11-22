35404a4acf87b33706e6daf5425fa74c78e34e63
Cristian Londoño Franco <clondonof@unal.edu.co>
2020-11-22 17:23:53 -0500

    diseñoClase

--- PANDACADEMY/src/vista/VentanaUsuario.java
+++ PANDACADEMY/src/vista/VentanaUsuario.java
@@ -7,83 +7,75 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
-public class VentanaUsuario  {
+public class VentanaUsuario {
 
-	VBox barra; 
-	Scene user;
-	MenuItem salir;
-	MenuItem editarProfesores;
+	BorderPane nombre = new BorderPane();
+	Scene user=new Scene(nombre,800,700);
 
-	public VentanaUsuario() {
-		
-		BorderPane nombre = new BorderPane();
-		user = new Scene(nombre, 800, 700);
-		
-		MenuBar barraMenu = new MenuBar();
-		
-		Menu archivo = new Menu("Archivo");
-		MenuItem usuarioMenu = new MenuItem("Usuario");
-		 salir = new MenuItem("Salir");
-		archivo.getItems().addAll(usuarioMenu, salir);
-		
-		Menu procesos = new Menu("Procesos y consultas");
-		
-		Menu perfil = new Menu("Perfil");
-		MenuItem mostrarPerfil = new MenuItem("Mostrar perfil"); 
-		MenuItem editarPerfil = new MenuItem("Editar Perfil");
-		perfil.getItems().addAll(mostrarPerfil, editarPerfil);
-		
-		Menu asignatura = new Menu("Asignatura");
-		MenuItem mostrarAsignaturas = new MenuItem("Mostrar Asignaturas"); 
-		MenuItem editarAsignaturas = new MenuItem("Editar Asignaturas");
-		MenuItem crearAsignaturas = new MenuItem("Crear Asignaturas");
-		MenuItem borrarAsignaturas = new MenuItem("Borrar Asignaturas");
-		asignatura.getItems().addAll(mostrarAsignaturas, editarAsignaturas, crearAsignaturas, borrarAsignaturas);
+	MenuBar barraMenu = new MenuBar();
 
-		
-		Menu notas = new Menu("Notas");
-		MenuItem mostrarNotas = new MenuItem("Mostrar notas"); 
-		MenuItem calcularPAPA = new MenuItem("Calcular PAPA"); 
-		MenuItem calculadora = new MenuItem("Calculadora"); 
-		notas.getItems().addAll(mostrarNotas, calcularPAPA, calculadora);
+	Menu archivo = new Menu("Archivo");
+	MenuItem usuarioMenu = new MenuItem("Usuario");
+	MenuItem salir=new MenuItem("Salir");
 
-		
-		Menu horario = new Menu("Horario");
-		MenuItem horariosClase = new MenuItem("Mostrar horarios de clase"); 
-		MenuItem horariosAsesoria = new MenuItem("Mostrar horarios de asesoría"); 
-		horario.getItems().addAll(horariosClase, horariosAsesoria);
+	Menu procesos = new Menu("Procesos y consultas");
 
-		
-		Menu profesores = new Menu("Profesores");
-		MenuItem listaProfesores = new MenuItem("Lista profesores");
-		MenuItem editarProfesores = new MenuItem("Editar profesores");
-		profesores.getItems().addAll(listaProfesores, editarProfesores);
+	Menu perfil = new Menu("Perfil");
+	MenuItem mostrarPerfil = new MenuItem("Mostrar perfil");
+	MenuItem editarPerfil = new MenuItem("Editar Perfil");
+	
 
-		
-		Menu semestres = new Menu("Semestres");
-		MenuItem nuevoSemestre = new MenuItem("Nuevo semestre");
-		MenuItem editarSemestre = new MenuItem("Editar semestre");
-		MenuItem eliminarSemestre = new MenuItem("Eliminar semestre");
-		MenuItem CambiarSemestre = new MenuItem("Cambiar semestre");
-		semestres.getItems().addAll(nuevoSemestre, editarSemestre, eliminarSemestre, CambiarSemestre);
+	Menu asignatura = new Menu("Asignatura");
+	MenuItem mostrarAsignaturas = new MenuItem("Mostrar Asignaturas");
+	MenuItem editarAsignaturas = new MenuItem("Editar Asignaturas");
+	MenuItem crearAsignaturas = new MenuItem("Crear Asignaturas");
+	MenuItem borrarAsignaturas = new MenuItem("Borrar Asignaturas");
+	
+
+	Menu notas = new Menu("Notas");
+	MenuItem mostrarNotas = new MenuItem("Mostrar notas");
+	MenuItem calcularPAPA = new MenuItem("Calcular PAPA");
+	MenuItem calculadora = new MenuItem("Calculadora");
+
+	Menu horario = new Menu("Horario");
+	MenuItem horariosClase = new MenuItem("Mostrar horarios de clase");
+	MenuItem horariosAsesoria = new MenuItem(
+			"Mostrar horarios de asesoría");
+
+	Menu profesores = new Menu("Profesores");
+	MenuItem listaProfesores = new MenuItem("Lista profesores");
+	MenuItem editarProfesores = new MenuItem(
+			"Editar profesores");
+
+	Menu semestres = new Menu("Semestres");
+	MenuItem nuevoSemestre = new MenuItem("Nuevo semestre");
+	MenuItem editarSemestre = new MenuItem("Editar semestre");
+	MenuItem eliminarSemestre = new MenuItem("Eliminar semestre");
+	MenuItem CambiarSemestre = new MenuItem(
+			"Cambiar semestre");
 
 
+	Menu ayuda = new Menu("Ayuda");
+	MenuItem acerca = new MenuItem("Acerca de");
+
+	
+	VBox barra = new VBox(barraMenu);
+
+	public VentanaUsuario(){
 		
-		procesos.getItems().addAll(perfil, asignatura, notas, horario, profesores, semestres);
-		
-		Menu ayuda = new Menu("Ayuda");
-		MenuItem acerca = new MenuItem("Acerca de");
+		archivo.getItems().addAll(usuarioMenu,salir);
+		perfil.getItems().addAll(mostrarPerfil,editarPerfil);
+		asignatura.getItems().addAll(mostrarAsignaturas,editarAsignaturas,crearAsignaturas,borrarAsignaturas);
+		notas.getItems().addAll(mostrarNotas,calcularPAPA,calculadora);
+		horario.getItems().addAll(horariosClase,horariosAsesoria);
+		profesores.getItems().addAll(listaProfesores,editarProfesores);
+		semestres.getItems().addAll(nuevoSemestre,editarSemestre,eliminarSemestre,CambiarSemestre);
+		procesos.getItems().addAll(perfil,asignatura,notas,horario,profesores,semestres);
 		ayuda.getItems().add(acerca);
-		
-		
-		barraMenu.getMenus().addAll(archivo, procesos, ayuda);
-		VBox barra = new VBox(barraMenu); 
-		
-		
-		nombre.setTop(barra);
 
-		
-		
+		barraMenu.getMenus().addAll(archivo,procesos,ayuda);
+
+		nombre.setTop(barra);
 	}
 
 }
