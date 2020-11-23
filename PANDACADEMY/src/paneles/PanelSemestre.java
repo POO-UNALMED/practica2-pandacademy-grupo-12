a9b736486a8e88bec6b169703b3e4ebb2bf00b3f
Juan Pablo Gutierrez Tamayo <jugutierrezt@unal.edu.co>
2020-11-23 13:24:17 -0500

    PanelSemestre 1.0

--- PANDACADEMY/src/paneles/PanelSemestre.java
+++ PANDACADEMY/src/paneles/PanelSemestre.java
@@ -2,11 +2,14 @@ package paneles;
 
 import gestorAplicacion.Academico.Semestre;
 import gestorAplicacion.Persona.Estudiante;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 public class PanelSemestre {
@@ -18,11 +21,23 @@ public class PanelSemestre {
 	Label desc= new Label("EN ESTA SECCION CAMBIAR DE SEMESTRE, AGREGAR NUEVOS SEMESTRES Y BORRAR SEMESTRES \n\nClick en el semestre para cambiar de semestre");
 	
 	GridPane principal = new GridPane();
+	ScrollPane scroll= new ScrollPane(principal);
+	GridPane p= new GridPane();
+	
 	
 	public PanelSemestre(){
 		
-		est.addSemestre(new Semestre("semestre 1"));
+		p.addColumn(0, scroll);
+		p.setAlignment(Pos.CENTER);
+		scroll.setPadding(new Insets(10));
+		
+		est.addSemestre(new Semestre("semestre 3"));
+		est.addSemestre(new Semestre("semestre 3s"));
+		est.addSemestre(new Semestre("semestre s"));
+		est.addSemestre(new Semestre("semestre s2"));
+		est.addSemestre(new Semestre("semestre sds"));
 		est.addSemestre(new Semestre("semestre 2"));
+		
 		principal.setAlignment(Pos.CENTER);
 		desc.setStyle("-fx-border-color: BLUE;");
 		titulo.setStyle("-fx-border-color: BLUE;");
@@ -30,6 +45,7 @@ public class PanelSemestre {
 		principal.setHgap(20);
 		VBox cajabot= new VBox();
 		cajabot.setSpacing(10);
+		
 		for (int i=0; i<est.getSemestres().size();i++) {
 			Button boton= new Button(est.getSemestres().get(i).getNombre());
 			cajabot.getChildren().add(boton);
@@ -44,7 +60,7 @@ public class PanelSemestre {
 	}
 	
 	public GridPane getPanel() {
-		return principal;
+		return p;
 	}
 
 }
